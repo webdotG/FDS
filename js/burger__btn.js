b8e821aeeae3e45410e5f2fa293ab46ea8f30309
webdotG <webdotg@gmail.com>
2022-07-01 16:18:14 +0300

    nav mobile header title visibility-hidden add

--- js/burger__btn.js
+++ js/burger__btn.js
@@ -5,7 +5,10 @@ function myFunction(x) {
 let navBtn = document.querySelector(".nav-btn");
 let navList = document.querySelector(".nav-list");
 let header = document.querySelector("header");
+let headerTitle = document.querySelector(".header__title-wrapper");
+
 navBtn.addEventListener('click', function () {
     navList.classList.toggle('nav-list--show');
     header.classList.toggle('background-black');
+    headerTitle.classList.toggle('visibility-hidden');
 })
