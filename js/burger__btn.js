ac3b5de86678a82bcf6c4e63e4ae94dde3897a54
webdotG <webdotg@gmail.com>
2022-07-01 14:45:25 +0300

    nav mobile version visibility hidden add and animation visibilirt visivble

--- js/burger__btn.js
+++ js/burger__btn.js
@@ -1,3 +1,10 @@
 function myFunction(x) {
     x.classList.toggle("change-btn");
 }
+
+let navBtn = document.querySelector(".nav-btn");
+let navList = document.querySelector(".nav-list");
+
+navBtn.addEventListener('click', function () {
+    navList.classList.toggle('nav-list--show');
+})
