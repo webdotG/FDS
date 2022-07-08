47ba700aaee6c0fe981c9af2a1f710b226b85630
webdotG <webdotg@gmail.com>
2022-07-08 12:06:43 +0300

    add section video and skills

--- js/color__change.js
+++ js/color__change.js
@@ -1,8 +1,25 @@
 const about = document.getElementById("about");
 const whyWe = document.getElementById("why-we");
 
+
 window.addEventListener('scroll', (evt) => {
-    changeColor(about, '#ff6347')
+    changeColor(about, '#151A51')
+})
+
+
+function changeColor(elem,color) {
+    console.log(window.scrollY, elem.getBoundingClientRect());
+    console.log(elem.getBoundingClientRect().y - window.scrollY);
+    const diff = elem.getBoundingClientRect().y - window.scrollY;
+    if (diff <= elem.getBoundingClientRect().height / 0.3 && diff >= 0) {
+        elem.style.background = color;
+    } else {
+        elem.style.background = '#000000';
+    }
+}
+
+window.addEventListener('scroll', (evt) => {
+    changeColor(whyWe, '#151A51')
 })
 
 
