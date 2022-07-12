c8ef50749c20bf5d8c009ee3b65b6de8c968f21a
webdotG <webdotg@gmail.com>
2022-07-12 15:42:08 +0300

    add script input file custom style

--- js/color__change.js
+++ js/color__change.js
@@ -33,3 +33,4 @@ function changeColor(elem,color) {
         elem.style.background = '#000000';
     }
 }
+
---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+
+console.log('alert');
+
+
+function updateSize() {
+    let nBytes = 0,
+        oFiles = this.files,
+        nFiles = oFiles.length;
+    for (let nFileId = 0; nFileId < nFiles; nFileId++) {
+        nBytes += oFiles[nFileId].size;
+    }
+    let sOutput = nBytes + " bytes";
+    const aMultiples = ["KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
+    for (nMultiple = 0, nApprox = nBytes / 1024; nApprox > 1; nApprox /= 1024, nMultiple++) {
+        sOutput = nApprox.toFixed(3) + " " + aMultiples[nMultiple] + " (" + nBytes + " bytes)";
+    }
+    document.getElementById("fileNum").innerHTML = nFiles;
+    document.getElementById("fileSize").innerHTML = sOutput;
+    document.getElementById('file--text').innerHTML='';
+}
+
+document.getElementById("file--input").addEventListener("change", updateSize, false);
